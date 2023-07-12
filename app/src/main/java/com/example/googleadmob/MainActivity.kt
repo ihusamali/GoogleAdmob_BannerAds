f393ff845969225b3b09c3bf5b68f394f4fbe87a
ihusamali <husamali6300@gmail.com>
2023-07-12 11:53:26 +0500

    Initial commit

---
+++ app/src/main/java/com/example/googleadmob/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.googleadmob
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.LoadAdError
+import com.google.android.gms.ads.MobileAds
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var mAdView : AdView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        loadBannerAd()
+    }
+
+    private fun loadBannerAd() {
+        MobileAds.initialize(this) {}
+
+        mAdView = findViewById(R.id.adView)
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
+
+        mAdView.adListener = object: AdListener() {
+            override fun onAdClicked() {
+                // Code to be executed when the user clicks on an ad.
+                Toast.makeText(this@MainActivity, "Ad Clicked", Toast.LENGTH_LONG).show()
+            }
+
+            override fun onAdClosed() {
+                // Code to be executed when the user is about to return
+                // to the app after tapping on an ad.
+                Toast.makeText(this@MainActivity, "Ad Closed", Toast.LENGTH_LONG).show()
+
+            }
+
+            override fun onAdFailedToLoad(adError : LoadAdError) {
+                // Code to be executed when an ad request fails.
+            }
+
+            override fun onAdImpression() {
+                // Code to be executed when an impression is recorded
+                // for an ad.
+            }
+
+            override fun onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+                Toast.makeText(this@MainActivity, "Ad Loaded", Toast.LENGTH_LONG).show()
+
+            }
+
+            override fun onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+                Toast.makeText(this@MainActivity, "Ad Opened", Toast.LENGTH_LONG).show()
+
+            }
+        }
+    }
+}
